dddcb2e5142e456eb4125573a30b2a471f4a7bf2
Samuel <samuelteweldebrhan@gmail.com>
2025-01-14 21:05:17 -0500

    Initial commit

--- Challenge2/Program.cs
+++ Challenge2/Program.cs
@@ -13,3 +13,23 @@ namespace Challenge2
         }
     }
 }
+
+public class EventSubscriber
+{
+    // Event handler method
+    public void OnMyEventOccurred(object sender, MyEventArgs e)
+    {
+        Console.WriteLine($"Event received: {e.Message}");
+    }
+}
+
+public class MyEventArgs : EventArgs
+{
+    public string Message { get; }
+
+    // Constructor to initialize the Message
+    public MyEventArgs(string message)
+    {
+        Message = message;
+    }
+}
